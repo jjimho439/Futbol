9041bc92b60bdbe979bfe65f0061d59168581262
vlopmun831 <vanesaopos81@gmail.com>
2025-03-04 12:40:46 +0100

    sub

--- src/Main.java
+++ src/Main.java
@@ -5,41 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int opcion;
-		do {
-			System.out.println("----Creación de equipos y jugadores----");
-			System.out.println("Seleccione una opción:");
-			System.out.println("1. Crear jugador");
-			System.out.println("2. Mostrar alineación ");
-			System.out.println("0.Salir ");
-		
-			opcion = sc.nextInt();
-
-			System.out.println();
-			System.out.println();
-
-			switch (opcion) {
-			case 1:
-				System.out.print("Nombre del jugador: ");
-                String nombre = sc.nextLine();
-                System.out.print("Dorsal: ");
-                int dorsal = sc.nextInt();
-                sc.nextLine();
-                System.out.print("Posición (Portero, Central, Lateral): ");
-                String posicion = sc.nextLine();
-               
-            case 0:
-                System.out.println("Saliendo del programa...");
-                break;
-            default:
-                System.out.println("Opción no válida");
-        }
-    } while (opcion != 0);
-
-  
+	
 }
 				
 	}
--- src/equipo/jugadores/portero/Portero.java
+++ src/equipo/jugadores/portero/Portero.java
@@ -21,19 +21,15 @@ abstract class Portero extends Jugador{
 	}
 
 	@Override
-<<<<<<< HEAD
+
 	public void mostrarDatos() {
 		System.out.println(super.toString() + ", Paradas: " + this.paradas);
 
 }
 	
 	
-=======
-	public String toString() {
-		return super.toString() + ", Paradas: "+paradas;
->>>>>>> ebb451da9c788972bb5020f2c0d9e320e25bfd7e
-		
+
 	}
 	
 
-}
+
